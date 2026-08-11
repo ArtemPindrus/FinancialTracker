9391ff1c92f0ec871def10e2e8c90110b82e8927
Artem Pindrus <artempindrus@gmail.com>
2026-08-11 19:54:33 +0300

    set design time datacontext

--- FinancialTracker/Views/FinanceUpdateView.axaml.cs
+++ FinancialTracker/Views/FinanceUpdateView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using FinancialTracker.Models;
+using FinancialTracker.ViewModels;
 
 namespace FinancialTracker.Views;
 
@@ -9,5 +11,12 @@ public partial class FinanceUpdateView : UserControl
     public FinanceUpdateView()
     {
         InitializeComponent();
+
+        if (Design.IsDesignMode) {
+            FinanceRecordDto record = new() { Tags = ["Tag1", "Tag2"] };
+            FinanceUpdateViewModel vm = new(record, ["Tag1", "Tag2", "Tag3", "Tag4"]);
+
+            Design.SetDataContext(this, vm);
+        }
     }
 }
